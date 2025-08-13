4669578b84882be37fd8a458fe08c13b5c4627e6
jordispir <jordispir@gmail.com>
2025-08-13 17:14:39 +0200

    formato fecha arreglado y excel también

--- routes/fichaje.js
+++ routes/fichaje.js
@@ -142,8 +142,8 @@ const now = new Date();
   const month = String(now.getMonth() + 1).padStart(2, '0');
   const year = String(now.getFullYear()).slice(-2); // últimos 2 dígitos
   const fecha = `${day}-${month}-${year}`; // 25-13-08
-  
-  const fileName = `fichajes-${fecha}.xlsx`;
+
+  const fileName = `fichajes${almacenId}-${fecha}.xlsx`;
   const s3Key = `${almacenId}/excel/${fecha}/${fileName}`;
 
   const buffer = await workbook.xlsx.writeBuffer();
